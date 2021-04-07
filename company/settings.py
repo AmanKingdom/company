c7a58e0d3daa4d8a3671c97e1b3d86616b3a378f
aman <1424675162@qq.com>
2021-04-07 12:37:44 +0800

    xxx

--- company/settings.py
+++ company/settings.py
@@ -115,10 +115,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-# STATIC_ROOT = '/home/projects/labs_system/static'
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'static'),
+#]
+STATIC_ROOT = '/home/projects/company/static'
 
 
 # 设置静态文件路径为主目录下的media文件夹
